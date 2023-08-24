d8af7ce6abbf389d0388c2d9ba2b5db7bee5033c
magleb <maximovglebigorevich@gmail.com>
2023-08-24 15:27:31 +0400

    add prints to main

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import os
 from core.solution.q_table_agent.train import train as train_q_table
 from core.utils.environment import Environment
 
-# learn: 100.000 * 100 = 10^7 * 1000 tests = 10^10
-
 NUM_EPISODES = 100
 MAX_STEPS = 86400
 ELEVATOR_MAX_WEIGHT = 680
 
+print(f"Training settings: NUM_EPISODES = {NUM_EPISODES},"
+      f" MAX_STEPS = {MAX_STEPS}, ELEVATOR_MAX_WEIGHT = {ELEVATOR_MAX_WEIGHT}")
+print(f"Total tests to be processed: {Environment.NUMBER_TEST_PER_CASE}")
+
 for i in range(Environment.NUMBER_TEST_PER_CASE):
+    print(f"\nProcessing test {i + 1} out of {Environment.NUMBER_TEST_PER_CASE}...")
+
     # READ TEST
     commands = []
     case_path = Environment.get_case_path(0)
     test_path = os.path.join(case_path, f'tests/test{i}.txt')
+    print(f"Reading test data from {test_path}")
     with open(test_path, 'r') as f:
         line = f.readline()
         n, levels, _, _, _, _ = line.split()
@@ -28,12 +33,21 @@ for i in range(Environment.NUMBER_TEST_PER_CASE):
             commands.append([time, from_level, to_level, weight])
             n -= 1
 
+    print(f"Read {len(commands)} commands from test data.")
+
     # LEARN
     agent_path = os.path.join(case_path, 'q_table_agent')
+    print(f"Starting training Q-Table agent...")
     reward = train_q_table(commands, levels, MAX_STEPS, NUM_EPISODES, ELEVATOR_MAX_WEIGHT, agent_path)
+    print(f"Training completed with final reward: {reward[-1] if reward else 'N/A'}")
+
     # SAVE RESULTS
     results_path = os.path.join(case_path, f'results')
     os.makedirs(results_path, exist_ok=True)
     result_path = os.path.join(results_path, f'result{i}.txt')
+    print(f"Saving results to {result_path}")
     with open(result_path, 'w') as f:
         f.writelines(map(lambda x: str(x) + '\n', reward))
+    print(f"Test {i + 1} processing completed!")
+
+print("\nAll tests processed successfully!")
